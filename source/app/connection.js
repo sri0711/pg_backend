9cb64d3b1f350c573b7e5fb0613c402b9fecf080
sri0711 <m.sathish0711@gmail.com>
2024-05-05 01:00:12 +0530

    Initial commit

--- source/app/connection.js
+++ source/app/connection.js
@@ -1,6 +1,6 @@
 const connection = {
 	establish: (app) => {
-		app.listen(3000, () => {
+		app.listen(process.env.PORT||3000, () => {
 			console.log('application started on http://localhost:3000');
 		});
 	},
